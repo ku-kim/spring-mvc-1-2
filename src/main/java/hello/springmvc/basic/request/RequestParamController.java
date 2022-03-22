4b382282dbe29457e9928485158eca360262f6dd
“kukim” <kukim.dev@gmail.com>
2022-03-22 22:57:40 +0900

    6.스프링MVC-기본기능: HTTP 요청 파라미터 - GET 쿼리 파라미터, POST HTTP Form
    - 클라이언트에서 서버로 요청데이터 전달할 때 3가지 방법(GET 쿼리 파라미터, POST - HTML Form, Json) 중 1,2 번 예시

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@Slf4j
+public class RequestParamController {
+
+    /**
+     * 반환 타입이 없으면서 이렇게 응답에 값을 직접 집어넣으면, view 조회X
+     */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse
+            response) throws IOException { // 예외는 response.getWriter 할 때 필요
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+        response.getWriter().write("ok");
+    }
+
+}
