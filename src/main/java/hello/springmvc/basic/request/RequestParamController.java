24365be8519f084ad24a1c4ace4b581e36eac280
“kukim” <kukim.dev@gmail.com>
2022-03-23 08:48:25 +0900

    6.스프링MVC-기본기능: HTTP 요청 파라미터 - @ModelAttribute
    - 요청 파라미터(쿼리 파라미터, HTML Form)의 입력일 때 @ModelAttribute 활용해 클라이언트 입력을 객체로 한 번에 받아오기

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -79,4 +80,17 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) { // 생략 가능
+        log.info("username={}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
 }
